b185edc0da551891926fb5fe361b060b4802512c
Alfred de Vulpian <derfladv@gmail.com>
2024-10-20 18:59:00 -0400

    feat: center calculation for every detected object with yolov11, little readme update

--- README.md
+++ README.md
@@ -1,30 +1,28 @@
-# Project Title
+# Smart Fridge (**WIP**)
 
 This project involves developing a smart fridge that tracks the items stored inside and suggests recipes based on the available ingredients.
 
 ## Getting Started
 
 Clone the project on your machine
+
 ```
 git clone https://github.com/Alfred0404/Smart_Fridge_Project_Code.git
 ```
 
 ### Prerequisites
 
-You need [Python >3.11](https://www.python.org/downloads/) and some dependencies:
+You need [Python &gt;3.11](https://www.python.org/downloads/) and some dependencies:
 
 - [Flask](https://flask.palletsprojects.com/en/3.0.x/)
 - [Ollama](https://github.com/ollama/ollama-python)
 - [OpenCV](https://vovkos.github.io/doxyrest-showcase/opencv/sphinx_rtd_theme/index.html#)
+- [Ultralytics](https://docs.ultralytics.com/quickstart/#install-ultralytics)
+- [EasyOCR](https://pypi.org/project/easyocr/)
 
 ```
-pip install opencv-python ollama Flask
+pip install opencv-python ollama Flask ultralytics easyocr
 ```
-
-## Built With
-
-- [Flask](https://flask.palletsprojects.com/en/3.0.x/)
-- [Tailwind](https://tailwindcss.com/)
 
 ## Authors
 
--- src/vision/yolov11_test.py
+++ src/vision/yolov11_test.py
@@ -1,10 +1,60 @@
 from ultralytics import YOLO
+import cv2
+import os
 
 
-def test_yolov11():
-    yolo_model_path = "C:\\Users\\ASUS ROG ALFRED\\Documents\\ECE\\ING3\\New York\\Courses\\Intelligent Robotics\\Project_SmartFridge\\Smart_Fridge_Project_Code\\src\\vision\\models\\yolo11n.pt"
-    model = YOLO(yolo_model_path)
-    results = model.predict(source=0, show=True)
+def calculate_center(x1, y1, x2, y2):
+    return (x1 + x2) / 2, (y1 + y2) / 2
+
+
+def is_moving_forward(center_1, center_2):
+    # check if an item is moving forward (from top to bottom)
+    _, y1 = center_1
+    _, y2 = center_2
+
+    return True if y1 < y2 else False
+
+
+def destroy(cap):
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def test_yolov11(model):
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        results = model.predict(source=frame, conf=0.4, show=False)
+
+        for result in results:
+            for box in result.boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x_center, y_center = calculate_center(x1, y1, x2, y2)
+                cv2.circle(frame, (int(x_center), int(y_center)), 3, (0, 255, 0), -1)
+                cv2.putText(
+                    frame,
+                    f"{int(x_center)},{int(y_center)}",
+                    (int(x_center), int(y_center)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 255, 0),
+                    1,
+                )
+
+        cv2.imshow("YOLOv11 Detection", results[0].plot())
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    destroy(cap)
+
 
 if __name__ == "__main__":
-    test_yolov11()
+    working_dir = os.getcwd().replace("\\", "\\\\")
+    yolo_model_path = f"{working_dir}\\src\\vision\\models\\yolo11n.pt"
+    model = YOLO(yolo_model_path)
+    test_yolov11(model)
